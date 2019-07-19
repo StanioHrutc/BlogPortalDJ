0aedb1109e123e0c78ab1c27119ceba7259418ff
Stanislav Hrytcyshyn <grucushun90@gmail.com>
2019-07-19 13:42:41 +0300

    Fixed error in manage.py file, now launching works well

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stories.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogdjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
