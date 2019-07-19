69438633947a11a7850353fae4dca1c301abe291
Stanislav Hrytcyshyn <grucushun90@gmail.com>
2019-07-19 14:36:11 +0300

    Fixed routing.Now awailable to route through stories/about/contact

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from stories import urls
+
+
+
 urlpatterns = [
-    path('', include('stories_index.urls')),
+    path('', include('stories.urls')),
     path('admin/', admin.site.urls),
 ]
--- stories/urls.py
+++ stories/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+import contact.views
+
 urlpatterns = [
     path('', views.index, name='index'), # root path
     path('story/<int:story_id>', views.story, name='story'),
     path('about', views.about, name='about'),
+    path('contact', contact.views.contact, name='contact'),
 ]
